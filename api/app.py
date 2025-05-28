25329cff2d0e6f6a6711b8b70920402521012457
Sammy Frankel <frankelsammy31@gmail.com>
2025-05-28 19:12:03 -0400

    update api

--- api/app.py
+++ api/app.py
@@ -2,22 +2,23 @@ from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-books = [
-    {"id": 1, "title": "1984", "author": "George Orwell"},
-    {"id": 2, "title": "To Kill a Mockingbird", "author": "Harper Lee"},
+payments = [
+    {"id": 1, "amount": 10.00},
+    {"id": 2, "amount": 2.00},
+    {"id": 3, "amount":  3.00}
 ]
 
 # GET all books
-@app.route('/books', methods=['GET'])
+@app.route('/payments', methods=['GET'])
 def get_books():
-    return jsonify(books)
+    return jsonify(payments)
 
-@app.route('/books', methods=['POST'])
-def add_book():
-    new_book = request.get_json()
-    new_book["id"] = max(book["id"] for book in books) + 1
-    books.append(new_book)
-    return jsonify(new_book), 201
+@app.route('/payments', methods=['POST'])
+def add_payment():
+    new_payment = request.get_json()
+    new_payment["id"] = max(payment["id"] for payment in payments) + 1
+    payments.append(new_payment)
+    return jsonify(new_payment), 201
 
 if __name__ == '__main__':
     app.run(debug=True)
